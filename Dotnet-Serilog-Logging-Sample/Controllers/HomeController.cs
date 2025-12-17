7c01a2486c7a6bc227d55399cc83271ae7004cdb
Mehmet Kıran <kiranmehmet62@gmail.com>
2025-12-17 18:07:40 +0300

    .

---
+++ Dotnet-Serilog-Logging-Sample/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dotnet_Serilog_Logging_Sample.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : ControllerBase
+    { 
+        private readonly ILogger<HomeController> _logger;
+          
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("trigger")]
+        public IActionResult Trigger()
+        {
+            throw new Exception("Test exception");
+        }
+          
+        [HttpGet("slow")]
+        public async Task<IActionResult> SlowEndpoint()
+        {
+            _logger.LogInformation("⏳ Slow endpoint started");
+            await Task.Delay(2000); // 2 saniye bekle
+            _logger.LogInformation("✅ Slow endpoint completed");
+            return Ok("Yavaş işlem tamamlandı!");
+        }
+
+        [HttpGet("user/{userId}")]
+        public IActionResult GetUser(int userId)
+        {
+            _logger.LogInformation("👤 User {UserId} requested", userId);
+
+            if (userId <= 0)
+            {
+                _logger.LogWarning("⚠️ Invalid user ID: {UserId}", userId);
+                return BadRequest("Geçersiz kullanıcı ID");
+            }
+
+            _logger.LogInformation("✅ User {UserId} data returned successfully", userId);
+            return Ok(new { UserId = userId, Name = $"User{userId}", Status = "Active" });
+        }
+         
+    }
+}
--- Dotnet-Serilog-Logging-Sample/Program.cs
+++ Dotnet-Serilog-Logging-Sample/Program.cs
@@ -1,17 +1,60 @@
+using Scalar.AspNetCore; 
+using Serilog;
+using Serilog.Formatting.Json;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .WriteTo.Console()
+     .WriteTo.File(
+        formatter: new JsonFormatter(),
+        path: "logs/app-.json",
+        rollingInterval: RollingInterval.Day)
+    .CreateLogger();
 
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Host.UseSerilog();
+
+builder.Services.AddControllers().AddApplicationPart(typeof(Program).Assembly); 
 builder.Services.AddOpenApi();
+
+builder.Services.AddEndpointsApiExplorer();
+ 
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+//Farklý bilgilerin loglanmasý için kullanýyoruz.
+app.UseSerilogRequestLogging(options =>
+{
+    options.EnrichDiagnosticContext = (context, httpContext) =>
+    {
+        context.Set("UserAgent", httpContext.Request.Headers.UserAgent);
+        context.Set("RemoteIP", httpContext.Connection.RemoteIpAddress);
+    };
+});
+
+app.Use(async (context, next) =>
+{
+    try
+    {
+        await next();
+    }
+    catch (Exception ex)
+    {
+        Log.Error(ex, "Unhandled exception occurred");
+
+        context.Response.StatusCode = 500;
+        await context.Response.WriteAsync("An unexpected error occurred.");
+    }
+});
+
+
+
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    //Swagger Benzeri Kütüphane için ekledim
+    app.MapOpenApi(); 
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
@@ -19,5 +62,6 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
 
 app.Run();
